9aa45b444bf1720b6f3388f6abe90afefccb95f8
roy-tian <tdjol@hotmail.com>
2019-05-23 11:30:46 +0800

    rearrange

--- include/royinit.h
+++ include/royinit.h
@@ -7,6 +7,7 @@
 #include <stddef.h>
 #include <stdbool.h>
 #include <string.h>
+#include <time.h>
 
 enum NumeralSystem {
   ROY_BINARY     = 2,
---
+++ roy.h
@@ -0,0 +1,14 @@
+#ifndef ROY_H
+#define ROY_H
+
+#include "include/royarray.h"
+#include "include/roystack.h"
+#include "include/royqueue.h"
+#include "include/royvector.h"
+#include "include/royslist.h"
+#include "include/roylist.h"
+#include "include/roydeque.h"
+#include "include/roynumber.h"
+#include "include/roystring.h"
+
+#endif // ROY_H
