7e9a6047ac2c7c059a77ae0cac6d1bdf255aaf18
Roy-Tian <tdjol@hotmail.com>
2019-05-27 00:33:12 +0800

    update

--- include/roymap.h
+++ include/roymap.h
@@ -1,8 +1,21 @@
-//
-// Created by Roy on 2019/5/26.
-//
+#ifndef ROYMAP_H
+#define ROYMAP_H
 
-#ifndef ROYLIB_ROYMAP_H
-#define ROYLIB_ROYMAP_H
+#include "royinit.h"
 
-#endif //ROYLIB_ROYMAP_H
+struct _RoyMap {
+  void           * key;
+  void           * value;
+  struct _RoyMap * left;
+  struct _RoyMap * right;
+};
+
+typedef struct _RoyMap RoyMap;
+
+RoyMap * roy_map_new(void);
+
+void roy_map_delete(RoyMap * map);
+
+RoyMap * roy_map_insert_sort(RoyMap * map, const void * key, size_t key_size, const void * value, size_t value_size, int (* comp)(const void *, const void *));
+
+#endif // ROYMAP_H
--- src/roymap.c
+++ src/roymap.c
@@ -1,5 +1,47 @@
-//
-// Created by Roy on 2019/5/26.
-//
+#include "../include/roymap.h"
 
-#include "roymap.h"
+RoyMap *
+roy_map_new(void) {
+  RoyMap * ret = malloc(sizeof(RoyMap));
+  ret->key   = NULL;
+  ret->value = NULL;
+  ret->left  = NULL;
+  ret->right = NULL;
+  return ret;
+}
+
+void
+roy_map_delete(RoyMap * map) {
+
+}
+
+RoyMap *
+roy_map_insert_sort(RoyMap     * map,
+                    const void * key,
+                    size_t       key_size,
+                    const void * value,
+                    size_t       value_size,
+                    int       (* comp)(const void *, const void *)) {
+  if (map->key == NULL) {
+    map->key = malloc(key_size);
+    memcpy(map->key, key, key_size);
+    map->value = malloc(value_size);
+    memcpy(map->value, value, value_size);
+  } else switch (comp(map->key, key)) {
+  case 0:
+    map->value = malloc(value_size);
+    memcpy(map->value, value, value_size);
+    break;
+  case 1:
+    map->left =
+      roy_map_insert_sort(map->left, key, key_size, value, value_size, comp);
+    break;
+  case -1:
+    map->right =
+      roy_map_insert_sort(map->right, key, key_size, value, value_size, comp);
+    break;
+  default:
+    break;
+  }
+  return map;
+}
