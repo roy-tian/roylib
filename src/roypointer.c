b66894c513761b7d17fa6d8c4ad21e7979bfbf0f
Roy Tian <tdjol@hotmail.com>
2019-07-10 16:23:09 +0800

    update

--- src/roypointer.c
+++ src/roypointer.c
@@ -2,7 +2,7 @@
 
 RoyPointer *
 roy_pointer_set(RoyPointer * pointer,
-                   void        * real_pointer) {
+                void       * real_pointer) {
   pointer->pointer = real_pointer;
   return pointer;
 }
