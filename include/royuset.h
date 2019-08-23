42d917d91c5c1583272714c49df95be3d66c2640
Roy Tian <tdjol@hotmail.com>
2019-08-23 17:53:31 +0800

    revise #endif

--- include/royuset.h
+++ include/royuset.h
@@ -2,13 +2,14 @@
 #define ROYUSET_H
 
 #include "royinit.h"
-#include "royslist.h"
+#include "royvector.h"
 
 struct _RoyUSet {
-  size_t  (* hash)(const void * elem, size_t set_size);
-  bool    (* equal)(const void * elem1, const void * elem2);
-  size_t     size;
-  void     * table;
+  size_t   (* hash)(const void * elem);
+  bool     (* equal)(const void * elem1, const void * elem2);
+  size_t      capacity;
+  size_t      element_size;
+  RoyVector * table;
 };
 
 // RoyUSet (aka 'Unordered Set' / 'Hash Set'): an associative container that contains a set of unique objects.
@@ -18,6 +19,8 @@ struct _RoyUSet {
 // Do not modify any elements, or it's hash could be changed and the container could be corrupted.
 typedef struct _RoyUSet RoyUSet;
 
+RoyUSet * roy_uset_new(size_t capacity, size_t element_size, size_t(* hash)(const void *),bool(* equal)(const void *, const void *));
 
+void roy_uset_delete(RoyUSet * uset);
 
 #endif // ROYUSET_H
