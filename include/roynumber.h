656aadaf63fb4bc33c632451d7b75acf3f5172bc
tdjol@hotmail.com <Tc3k8-yb46t>
2019-08-25 15:16:13 +0800

    unsigned long long -> uint64_t

--- include/roynumber.h
+++ include/roynumber.h
@@ -2,18 +2,19 @@
 #define ROYNUMBER_H
 
 #include "royinit.h"
+#include <stdint.h>
 
 // Converts 'str' of binary digits into its equivalent unsigned value.
 // (The parsing phase ends immediately when 'str' turned to be ill-formed.)
-unsigned long long roy_parse_binary(const char * str);
+uint64_t roy_parse_binary(const char * str);
 
 // Converts 'str' of octal digits into its equivalent unsigned value.
 // (The parsing phase ends immediately when 'str' turned to be ill-formed.)
-unsigned long long roy_parse_octal(const char * str);
+uint64_t roy_parse_octal(const char * str);
 
 // Converts 'str' of hexadecimal digits into its equivalent unsigned value.
 // (The parsing phase ends immediately when 'str' turned to be ill-formed.)
-unsigned long long roy_parse_hexadecimal(const char * str);
+uint64_t roy_parse_hexadecimal(const char * str);
 
 // Converts 'str' of digits into its equivalent value.
 // (The parsing phase ends immediately when 'str' turned to be ill-formed.)
@@ -29,22 +30,22 @@ char * roy_llong_to_string(char * dest, long long number, size_t base, size_t wi
 
 // Converts the unsigned integer 'number' into a 'width'-width, 'base' character representation in the string 'dest'.
 // (The behavior is undefined if 'dest' or 'width' is insufficient, or 'base' is larger than 36 ([0-9a-z]).)
-char * roy_ullong_to_string(char * dest, unsigned long long number, size_t base, size_t width, bool fill_zero);
+char * roy_ullong_to_string(char * dest, uint64_t number, size_t base, size_t width, bool fill_zero);
 
 // Returns 'dest' with the 'count' bits that begin at position 'position' set to the rightmost 'count' bits of 'src', 
 // the other bits are left unchanged.
-unsigned long long roy_ullong_set_bits(unsigned long long * dest, int position, size_t count, unsigned long long src);
+uint64_t roy_ullong_set_bits(uint64_t * dest, int position, size_t count, uint64_t src);
 
 // Returns 'dest' with the 'count' bits that begin at position 'position' inverted 
 // (i.e., 1 changed into 0 and vice versa), the other bits are left unchanged.
-unsigned long long roy_ullong_invert(long long * number, int position, size_t count);
+uint64_t roy_ullong_invert(long long * number, int position, size_t count);
 
 // Returns the value of the 'width'-width unsigned integer 'number' rotated to the right by 'steps' bit positions.
-unsigned long long roy_ullong_rotate_right(unsigned long long * number, int steps, size_t width); 
+uint64_t roy_ullong_rotate_right(uint64_t * number, int steps, size_t width); 
 
 // Returns the value of the 'width'-width unsigned integer 'number' rotated to the left by 'steps' bit positions.
-unsigned long long roy_ullong_rotate_left(unsigned long long * number, int steps, size_t width); 
+uint64_t roy_ullong_rotate_left(uint64_t * number, int steps, size_t width); 
 
 // Counts '1' bits in unsigned integer 'number'.
-size_t roy_ullong_count_bit(unsigned long long number);
+size_t roy_ullong_count_bit(uint64_t number);
 #endif // ROYNUMBER_H
--- src/roynumber.c
+++ src/roynumber.c
@@ -19,9 +19,9 @@
 #define STR_TRIM_LEFT(str)        \
         str += strspn(str, " \t");
 
-unsigned long long
+uint64_t
 roy_parse_binary(const char * str) {
-  unsigned long long result = 0ULL;
+  uint64_t result = 0ULL;
   STR_TRIM_LEFT(str)
   size_t len = strspn(str, "01");
   for (size_t i = 0 ; i != len; i++) {
@@ -33,9 +33,9 @@ roy_parse_binary(const char * str) {
   return result;
 }
 
-unsigned long long
+uint64_t
 roy_parse_octal(const char * str) {
-  unsigned long long result = 0ULL;
+  uint64_t result = 0ULL;
   STR_TRIM_LEFT(str)
   STR_INC("0", str)
   size_t len = strspn(str, "01234567");
@@ -46,9 +46,9 @@ roy_parse_octal(const char * str) {
   return result; 
 }
 
-unsigned long long
+uint64_t
 roy_parse_hexadecimal(const char * str) {
-  unsigned long long result = 0ULL;
+  uint64_t result = 0ULL;
   STR_TRIM_LEFT(str)
   if (strstr(str, "0X") == str || strstr(str, "0x") == str) {
     str += 2;
@@ -135,7 +135,7 @@ roy_llong_to_string(char      * dest,
 
 char *
 roy_ullong_to_string(char               * dest,
-                     unsigned long long   number,
+                     uint64_t   number,
                      size_t               base,
                      size_t               width,
                      bool                 fill_zero) {
@@ -152,44 +152,44 @@ roy_ullong_to_string(char               * dest,
   return roy_str_reverse(dest);
 }
 
-unsigned long long
-roy_ullong_set_bits(unsigned long long * dest,
+uint64_t
+roy_ullong_set_bits(uint64_t * dest,
                     int                  position,
                     size_t               count,
-                    unsigned long long   src) {
+                    uint64_t   src) {
   return *dest = (*dest & ~(~(~0ULL << count) << (position + 1 - count))) |
                  ( src  &   ~(~0ULL << count) << (position + 1 - count));
 }
 
-unsigned long long
+uint64_t
 roy_ullong_invert(long long * number,
                   int         position,
                   size_t      count) {
   return *number = *number ^ ~(~0ULL << count) << (position + 1 - count);
 }
 
-unsigned long long
-roy_ullong_rotate_right(unsigned long long * number,
+uint64_t
+roy_ullong_rotate_right(uint64_t * number,
                         int                  steps,
                         size_t               width) {
-  unsigned long long right = (*number & ~(~0ULL << steps)) << (width - steps);
+  uint64_t right = (*number & ~(~0ULL << steps)) << (width - steps);
   *number >>= steps;
   *number |= right;
   return *number;
 }
 
-unsigned long long
-roy_ullong_rotate_left(unsigned long long * number,
+uint64_t
+roy_ullong_rotate_left(uint64_t * number,
                        int                  steps,
                        size_t               width) {
-  unsigned long long right = (*number & ~(~0ULL << (width - steps))) << steps;
+  uint64_t right = (*number & ~(~0ULL << (width - steps))) << steps;
   *number >>= width - steps;
   *number |= right;
   return *number;
 }
 
 size_t
-roy_ullong_count_bit(unsigned long long number) {
+roy_ullong_count_bit(uint64_t number) {
   size_t count = 0;
   // 'n & n - 1' deletes the rightmost '1' of n.
   for (; number != 0; number &= number - 1) { 
