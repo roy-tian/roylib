a23251e941fa78c39aa01e94c7d082fcca46b1f2
roy-tian <tdjol@hotmail.com>
2019-11-21 17:12:55 +0800

    upload shellsort

---
+++ src/shellsort.c
@@ -0,0 +1,10 @@
+#include "../include/shellsort.h"
+
+int
+gap_index(size_t n_elements) {
+  int index = 0;
+  while (!(GAPS[index] >= n_elements && GAPS[index + 1] <= n_elements)) {
+    index++;
+  }
+  return index;
+}
