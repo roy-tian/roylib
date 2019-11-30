9d6003f0243b4d8ace026b401f59cb7f2742e565
roy-tian <tdjol@hotmail.com>
2019-11-30 09:21:00 +0800

    newly upload

--- src/royumap.c
+++ src/royumap.c
@@ -1,1 +1,115 @@
 #include "royumap.h"
+
+RoyUMap *
+roy_umap_new(size_t   bucket_count, 
+             uint64_t seed, 
+             RHash    hash, 
+             RCompare comparer, 
+             ROperate deleter) {
+  RoyUMap * ret = (RoyUMap *)malloc(sizeof(RoyUMap));
+  ret->uset = roy_uset_new(bucket_count, seed, hash, comparer, deleter);
+  return ret;
+}
+
+void
+roy_umap_delete(RoyUMap * umap) {
+  roy_uset_delete(umap->uset);
+  free(umap);
+  umap = NULL;
+}
+
+const RoyPair *
+roy_umap_cpointer(const RoyUMap * umap,
+                  size_t          bucket_index,
+                  size_t          bucket_position) {
+  return roy_uset_cpointer(umap->uset, bucket_index, bucket_position);
+}
+
+size_t
+roy_umap_size(const RoyUMap * umap) {
+  return roy_uset_size(umap->uset);
+}
+
+bool
+roy_umap_empty(const RoyUMap * umap) {
+  return roy_uset_empty(umap->uset);
+}
+
+bool
+roy_umap_insert(RoyUMap * umap, 
+                void    * key,
+                size_t    key_size,
+                void    * value) {
+  RoySList ** node =
+    &umap->uset->buckets[roy_umap_bucket(umap, key, key_size)];
+  for (RoySList * iter = roy_slist_begin(*node); iter; iter = iter->next) {
+    if (umap->uset->comparer(roy_pair_key(iter->data), key) == 0) {
+      return false;
+    }
+  }
+  roy_slist_push_front(*node, roy_pair_new(key, value));
+  umap->uset->size++;
+  return true;
+}
+
+bool
+roy_umap_erase(RoyUMap * umap,
+               size_t    bucket_index,
+               size_t    bucket_position) {
+  return roy_uset_erase(umap->uset, bucket_index, bucket_position);
+}
+
+size_t
+roy_umap_remove(RoyUMap    * umap,
+                const void * key,
+                size_t       key_size) {
+  return roy_uset_remove(umap->uset, key, key_size);
+}
+
+void
+roy_umap_clear(RoyUMap * umap) {
+  return roy_uset_clear(umap->uset);
+}
+
+const void *
+roy_umap_find(const RoyUMap * umap,
+              const void    * key,
+              size_t          key_size) {
+  return (*(RoyPair *)roy_uset_find(umap->uset, key, key_size)).value;
+}
+
+size_t
+roy_umap_bucket_count(const RoyUMap * umap) {
+  return roy_uset_bucket_count(umap->uset);
+}
+
+size_t
+roy_umap_bucket_size(const RoyUMap * umap,
+                     size_t          bucket_index) {
+  return roy_uset_bucket_size(umap->uset, bucket_index);
+}
+
+int64_t
+roy_umap_bucket(const RoyUMap * umap,
+                const void    * key,
+                size_t          key_size) {
+  return roy_uset_bucket(umap->uset, key, key_size);
+}
+
+double
+roy_umap_load_factor(const RoyUMap * umap) {
+  return roy_uset_load_factor(umap->uset);
+}
+
+void
+roy_umap_for_each(RoyUMap * umap,
+                  ROperate  operate) {
+  roy_uset_for_each(umap->uset, operate);
+}
+
+void
+roy_umap_for_which(RoyUMap    * umap,
+                   RCondition   condition,
+                   ROperate     operate) {
+  roy_uset_for_which(umap->uset, condition, operate);
+}
--- src/royumap.h
+++ src/royumap.h
@@ -1,4 +1,89 @@
 #ifndef ROYUMAP_H
 #define ROYUMAP_H
 
+#include "trivials/royinit.h"
+#include "trivials/roypair.h"
+#include "royuset.h"
+
+struct RoyUMap_ {
+  RoyUSet * uset;
+};
+
+typedef struct RoyUMap_ RoyUMap;
+
+
+/* CONSTRUCTION & DESTRUCTION */
+
+// Returns a pointer to a newly build RoyUMap.
+// using a hash seed, a hash function and a comparer function(NULL if you want to use default versions).
+RoyUMap * roy_umap_new(size_t bucket_count, uint64_t seed, RHash hash, RCompare comparer, ROperate deleter);
+
+// De-allocates all the memory allocated.
+// (Always call this function after the work is done by the given 'umap', or memory leak will occur.)
+void roy_umap_delete(RoyUMap * umap);
+
+/* ELEMENT ACCESS */
+
+// Returns an pointer to the pair which is the 'bucket_position'-th one on 'bucket_index'-th buckets.
+// (Returns NULL if position is out of range.)
+const RoyPair * roy_umap_cpointer(const RoyUMap * umap, size_t bucket_index, size_t bucket_position);
+
+// Returns an typed pointer to the key which is the 'bucket_position'-th one on 'bucket_index'-th buckets.
+// (Returns NULL if position is out of range.)
+#define roy_umap_key_at(umap, bucket_index, bucket_position, key_type) \
+        (key_type *)(roy_umap_cpointer(umap, bucket_index, bucket_position)->key)
+
+#define roy_umap_value_at(umap, bucket_index, bucket_position, value_type) \
+        (value_type *)(roy_umap_cpointer(umap, bucket_index, bucket_position)->value)
+
+
+/* CAPACITY */
+
+// Returns the number of elements in 'umap'.
+size_t roy_umap_size(const RoyUMap * umap);
+
+// Returns whether there is any elements in 'umap'.
+bool roy_umap_empty(const RoyUMap * umap);
+
+/* MODIFIERS */
+
+// Hashes a pair conbined by 'key' and 'value' into 'umap'.
+bool roy_umap_insert(RoyUMap * umap, void * key, size_t key_size, void * value);
+
+// Removes a pair which is the 'bucket_position'-th one on 'bucket_index'-th buckets of 'umap'.
+bool roy_umap_erase(RoyUMap * umap, size_t bucket_index, size_t bucket_position);
+
+// Removes the pair in 'umap' of which 'key' is equal to given 'key'.
+size_t roy_umap_remove(RoyUMap * umap, const void * key, size_t key_size);
+
+// Removes all elements from 'umap'.
+void roy_umap_clear(RoyUMap * umap);
+
+/* LOOKUPS */
+
+// Finds the value of pair of which 'key' equivalent to given 'key'.
+const void * roy_umap_find(const RoyUMap * umap, const void * key, size_t key_size);
+
+/* HASH SET SPECIFIC */
+
+// Returns the number of buckets in 'umap'.
+size_t roy_umap_bucket_count(const RoyUMap * umap);
+
+// Returns the number of elements in the buckets with index 'bucket_index'.
+size_t roy_umap_bucket_size(const RoyUMap * umap, size_t bucket_index);
+
+// Returns the index of the buckets for key 'data' calculated by hash function of 'umap'.
+int64_t roy_umap_bucket(const RoyUMap * umap, const void * data, size_t data_size);
+
+// Returns the average number of elements per buckets.
+double roy_umap_load_factor(const RoyUMap * umap);
+
+/* TRAVERSE */
+
+// Traverses all elements in 'umap' using 'operate'.
+void roy_umap_for_each(RoyUMap * umap, ROperate oeprate);
+
+// Traverses all elements whichever meets 'condition' in 'umap' using 'operate'.
+void roy_umap_for_which(RoyUMap * umap, RCondition condition, ROperate operate);
+
 #endif // ROYUMAP_H
