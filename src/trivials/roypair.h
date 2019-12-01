02788d60c0b4cd850545c4b34bbdf385f0edf67f
roy-tian <tdjol@hotmail.com>
2019-12-01 22:48:38 +0800

    add #ifndef

--- src/trivials/roypair.h
+++ src/trivials/roypair.h
@@ -1,3 +1,6 @@
+#ifndef ROYPAIR_H
+#define ROYPAIR_H
+
 #include "royinit.h"
 
 struct RoyPair_ {
@@ -10,3 +13,5 @@ typedef struct RoyPair_ RoyPair;
 RoyPair * roy_pair_new(void * key, void * value);
 void * roy_pair_key(RoyPair * pair);
 void * roy_pair_value(RoyPair * pair);
+
+#endif // ROYPAIR_H
